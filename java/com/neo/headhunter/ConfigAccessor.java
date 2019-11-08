027b84c935dcef165e43b1c6ed61d2285bba663d
Asiru <12991517+Asiru@users.noreply.github.com>
2019-11-08 13:17:06 -0500

    Added multiple configuration access

---
+++ java/com/neo/headhunter/ConfigAccessor.java
@@ -0,0 +1,112 @@
+package com.neo.headhunter;
+
+import org.apache.commons.io.FileUtils;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.logging.Level;
+
+public class ConfigAccessor {
+	protected final JavaPlugin plugin;
+	private final boolean dynamic;
+	private final String fileName, ancestry;
+	
+	private File configFile;
+	protected FileConfiguration config;
+	
+	public ConfigAccessor(JavaPlugin plugin, boolean dynamic, String fileName, String... ancestry) {
+		this.plugin = plugin;
+		this.dynamic = dynamic;
+		this.fileName = fileName.endsWith(".yml") ? fileName : (fileName + ".yml");
+		this.ancestry = (ancestry == null || ancestry.length == 0) ? "" : String.join(File.separator, ancestry);
+		
+		// automatically load existing configuration or create config file when creating new object
+		reload();
+	}
+	
+	/**
+	 * Deletes the file associated with this configuration.
+	 */
+	public void delete() {
+		// ignore if config file has not yet been initialized
+		// * this should never happen
+		if(configFile != null && !configFile.delete())
+			plugin.getLogger().log(Level.SEVERE, "Could not delete " + configFile.getName());
+	}
+	
+	/**
+	 * Saves the current configuration to the specified file.
+	 */
+	public void save() {
+		// ignore if config file or configuration have not yet been initialized
+		// * this should never happen
+		if(configFile == null || config == null)
+			return;
+		try {
+			// save configuration to file
+			config.save(configFile);
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * If necessary, first initializes the configuration and associated file, then loads the configuration.
+	 */
+	public void reload() {
+		// create plugin data folder and subfolders if they don't exist
+		if(!plugin.getDataFolder().exists()) {
+			if(!plugin.getDataFolder().mkdir())
+				throw new IllegalStateException("plugin data folder could not be created");
+		}
+		File subdirs = new File(plugin.getDataFolder(), ancestry);
+		if(!subdirs.exists()) {
+			if(!subdirs.mkdirs())
+				throw new IllegalStateException("plugin subfolders could not be created");
+		}
+		
+		if(ancestry.isEmpty())
+			configFile = new File(plugin.getDataFolder(), fileName);
+		else
+			configFile = new File(plugin.getDataFolder() + File.separator + ancestry, fileName);
+		
+		try {
+			if (configFile.exists()) {
+				// if config file already exists, just load configuration
+				if (!dynamic)
+					FileUtils.copyInputStreamToFile(this.getResource(), configFile);
+				config = YamlConfiguration.loadConfiguration(configFile);
+			} else {
+				// if config file does not exist, create it appropriately
+				if (this.getResource() != null) {
+					// if config file has a default in the JAR
+					FileUtils.copyInputStreamToFile(this.getResource(), configFile);
+					config = YamlConfiguration.loadConfiguration(configFile);
+					config.setDefaults(YamlConfiguration.loadConfiguration(new InputStreamReader(this.getResource())));
+				} else {
+					// if config file does not have a default in the JAR
+					try {
+						if (!configFile.createNewFile())
+							plugin.getLogger().log(Level.SEVERE, "Could not create " + configFile.getName());
+					} catch (IOException ex) {
+						ex.printStackTrace();
+					}
+					config = YamlConfiguration.loadConfiguration(configFile);
+				}
+			}
+		} catch(IOException ex) {
+			plugin.getLogger().log(Level.SEVERE, "Error getting input stream for file: \"" + fileName + "\"");
+			ex.printStackTrace();
+		}
+	}
+	
+	// helper method for readability
+	private InputStream getResource() {
+		return plugin.getResource(fileName);
+	}
+}
