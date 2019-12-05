8e1621af1ddf3112feca099290f8f60124d9cc1a
Asiru <12991517+Asiru@users.noreply.github.com>
2019-12-04 23:28:36 -0500

    Reorganized head drops

--- java/com/neo/headhunter/manager/DropManager.java
+++ java/com/neo/headhunter/manager/DropManager.java
@@ -74,6 +74,16 @@ public class DropManager implements Listener {
 		return 0;
 	}
 	
+	private double getSmiteEffect(ItemStack weapon) {
+		// hunter's weapon's smite effect
+		if(weapon != null && weapon.containsEnchantment(Enchantment.DAMAGE_UNDEAD)) {
+			double smiteEffect = plugin.getSettings().getSmiteEffect();
+			return smiteEffect * weapon.getEnchantmentLevel(Enchantment.DAMAGE_UNDEAD);
+		}
+		return 0;
+	}
+	
+	// overall drop chance for PvP kills
 	private double getPlayerDropChance(Player hunter, ItemStack weapon, Player victim) {
 		double stealChance = getBasePlayerStealChance(hunter);
 		stealChance += getLootingEffect(weapon);
@@ -81,40 +91,53 @@ public class DropManager implements Listener {
 		return Math.min(1.0, stealChance * dropChance);
 	}
 	
+	// overall drop chance for PvE kills
 	private double getMobDropChance(Player hunter, ItemStack weapon, String mobConfigPath) {
 		double stealChance = getBaseMobStealChance(hunter, mobConfigPath);
 		stealChance += getLootingEffect(weapon);
 		return Math.min(1.0, stealChance);
 	}
 	
-	// proportion (0.0 - 1.0) of 'victim''s balance being imparted to a head if 'hunter' kills it with 'weapon'
-	private double getStealRate(Player hunter, ItemStack weapon, LivingEntity victim) {
-		double stealRate = getBaseStealBalance(hunter, victim);
-		
-		// hunter's weapon's smite effect
-		if(weapon != null && weapon.containsEnchantment(Enchantment.DAMAGE_UNDEAD)) {
-			double smiteEffect = plugin.getSettings().getSmiteEffect();
-			stealRate += (smiteEffect * weapon.getEnchantmentLevel(Enchantment.DAMAGE_UNDEAD));
-		}
-		
-		// victim's save effect
-		double victimRate = 1;
-		if(victim instanceof Player)
-			victimRate = getBaseDropBalance((Player) victim);
-		
-		return Math.min(1.0, stealRate * victimRate);
+	// overall drop balance for PvP kills
+	private double getPlayerDropBalance(Player hunter, ItemStack weapon, Player victim) {
+		double stealBalance = getBasePlayerStealBalance(hunter);
+		stealBalance += getSmiteEffect(weapon);
+		double dropBalance = getBasePlayerDropBalance(victim);
+		return Math.min(1.0, stealBalance * dropBalance);
 	}
 	
+	// overall drop balance for PvE kills
+	private double getMobDropBalance(Player hunter, ItemStack weapon, String mobConfigPath) {
+		double stealBalance = getBaseMobStealBalance(hunter, mobConfigPath);
+		stealBalance += getSmiteEffect(weapon);
+		return Math.min(1.0, stealBalance);
+	}
+	
+	// probability of the hunter causing a head to drop
 	private double getBasePlayerStealChance(Player hunter) {
 		Double stealChance = getPermissionValue(hunter, STEAL_CHANCE_PERM);
 		return stealChance != null ? stealChance : DEFAULT_STEAL_CHANCE;
 	}
 	
+	// probability of the victim player to drop a head
 	private double getBasePlayerDropChance(Player victim) {
 		Double dropChance = getPermissionValue(victim, DROP_CHANCE_PERM);
 		return dropChance != null ? dropChance : DEFAULT_DROP_CHANCE;
 	}
 	
+	// proportion of a victim's balance the hunter can steal
+	private double getBasePlayerStealBalance(Player hunter) {
+		Double stealBalance = getPermissionValue(hunter, STEAL_BALANCE_PERM);
+		return stealBalance != null ? stealBalance : DEFAULT_STEAL_BALANCE;
+	}
+	
+	// proportion of the victim's balance they will drop
+	private double getBasePlayerDropBalance(Player victim) {
+		Double dropBalance = getPermissionValue(victim, DROP_BALANCE_PERM);
+		return dropBalance != null ? dropBalance : DEFAULT_DROP_BALANCE;
+	}
+	
+	// probability of the hunter causing this mob's head to drop
 	private double getBaseMobStealChance(Player hunter, String mobConfigPath) {
 		if(mobConfigPath != null) {
 			String stealChancePerm = String.join(".", STEAL_CHANCE_PERM, mobConfigPath.toLowerCase());
@@ -125,20 +148,15 @@ public class DropManager implements Listener {
 		return plugin.getHeadLibrary().getDropChance(mobConfigPath);
 	}
 	
-	private double getBaseStealBalance(Player hunter, LivingEntity victim) {
-		String stealBalancePerm = STEAL_BALANCE_PERM;
-		if(!(victim instanceof Player)) {
-			String mobConfigPath = plugin.getHeadLibrary().getConfigPath(victim);
-			if(mobConfigPath != null)
-				stealBalancePerm = String.join(".", stealBalancePerm, mobConfigPath.toLowerCase());
+	// proportion of the mob's "balance" the hunter can steal
+	private double getBaseMobStealBalance(Player hunter, String mobConfigPath) {
+		if(mobConfigPath != null) {
+			String stealBalancePerm = String.join(".", STEAL_BALANCE_PERM, mobConfigPath.toLowerCase());
+			Double stealBalance = getPermissionValue(hunter, stealBalancePerm);
+			if(stealBalance != null)
+				return stealBalance;
 		}
-		Double stealBalance = getPermissionValue(hunter, stealBalancePerm);
-		return stealBalance != null ? stealBalance : DEFAULT_STEAL_BALANCE;
-	}
-	
-	private double getBaseDropBalance(Player victim) {
-		Double dropBalance = getPermissionValue(victim, DROP_BALANCE_PERM);
-		return dropBalance != null ? dropBalance : DEFAULT_DROP_BALANCE;
+		return DEFAULT_STEAL_BALANCE;
 	}
 	
 	// Generic method for checking permissions regardless of op
@@ -172,10 +190,10 @@ public class DropManager implements Listener {
 		
 		private void init() {
 			if(victim instanceof Player) {
-				Player victimPlayer = (Player) victim;
-				baseHead = plugin.getHeadLibrary().getPlayerHead(victimPlayer);
-				balanceValue = plugin.getEconomy().getBalance(victimPlayer);
-				bountyValue = plugin.getBountyManager().getTotalBounty(victimPlayer);
+				Player vPlayer = (Player) victim;
+				baseHead = plugin.getHeadLibrary().getPlayerHead(vPlayer);
+				balanceValue = getPlayerDropBalance(hunter, weapon, vPlayer) * plugin.getEconomy().getBalance(vPlayer);
+				bountyValue = plugin.getBountyManager().getTotalBounty(vPlayer);
 				withdrawValue = balanceValue;
 				if(plugin.getSettings().isCumulativeValue())
 					sellValue = balanceValue + bountyValue;
@@ -188,12 +206,12 @@ public class DropManager implements Listener {
 					sellValue = 0;
 					withdrawValue = 0;
 				}
-				dropChance = getPlayerDropChance(hunter, weapon, victimPlayer);
+				dropChance = getPlayerDropChance(hunter, weapon, vPlayer);
 			} else {
-				String mobConfigPath = plugin.getHeadLibrary().getConfigPath(victim);
-				baseHead = plugin.getHeadLibrary().getMobHead(mobConfigPath);
-				balanceValue = plugin.getHeadLibrary().getMaxPrice(mobConfigPath);
-				dropChance = getMobDropChance(hunter, weapon, mobConfigPath);
+				String path = plugin.getHeadLibrary().getConfigPath(victim);
+				baseHead = plugin.getHeadLibrary().getMobHead(path);
+				balanceValue = getMobDropBalance(hunter, weapon, path) * plugin.getHeadLibrary().getMaxPrice(path);
+				dropChance = getMobDropChance(hunter, weapon, path);
 			}
 		}
 		
