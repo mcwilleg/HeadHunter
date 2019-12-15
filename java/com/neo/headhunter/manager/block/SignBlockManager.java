af5ec8f91a7b686a6bb7cdb213858a78f7be397d
Asiru <12991517+Asiru@users.noreply.github.com>
2019-12-15 00:23:54 -0500

    Added bounty sign heads

--- java/com/neo/headhunter/manager/block/SignBlockManager.java
+++ java/com/neo/headhunter/manager/block/SignBlockManager.java
@@ -4,9 +4,14 @@ import com.neo.headhunter.HeadHunter;
 import com.neo.headhunter.bounty.BountyListEntry;
 import com.neo.headhunter.config.BlockConfigAccessor;
 import com.neo.headhunter.message.Message;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.block.Block;
+import org.bukkit.block.BlockState;
 import org.bukkit.block.Sign;
+import org.bukkit.block.Skull;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -16,14 +21,14 @@ import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockExplodeEvent;
 import org.bukkit.event.block.SignChangeEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.scheduler.BukkitRunnable;
 
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
+import java.util.*;
 
 public class SignBlockManager extends BlockConfigAccessor<HeadHunter> implements Listener {
 	private BountySignUpdateRunnable bountyRunnable;
+	private Map<Player, Sign> signLinkMap;
 	
 	public SignBlockManager(HeadHunter plugin) {
 		super(plugin, "signs.yml", "data");
@@ -31,23 +36,55 @@ public class SignBlockManager extends BlockConfigAccessor<HeadHunter> implements
 		this.bountyRunnable = new BountySignUpdateRunnable();
 		this.bountyRunnable.runTaskTimer(plugin, 0L, 20L);
 		requestUpdate();
+		
+		this.signLinkMap = new HashMap<>();
 	}
 	
 	@EventHandler(ignoreCancelled = true)
 	public void onPlayerInteract(PlayerInteractEvent event) {
 		if(event.getAction() != Action.RIGHT_CLICK_BLOCK)
 			return;
+		if(!plugin.isVersionBefore(1, 9, 0) && event.getHand() != EquipmentSlot.HAND)
+			return;
 		Block block = event.getClickedBlock();
+		if(block == null)
+			return;
+		Player hunter = event.getPlayer();
+		
 		String ownerUUID = (String) getBlockData("sell-head", block, "owner");
-		if(ownerUUID != null && block != null && block.getState() instanceof Sign) {
+		if(ownerUUID != null && block.getState() instanceof Sign) {
 			// check permission
-			if(!event.getPlayer().hasPermission("hunter.sellhead.sign")) {
-				event.getPlayer().sendMessage(Message.PERMISSION.format("selling at signs"));
+			if(!hunter.hasPermission("hunter.sellhead.sign")) {
+				hunter.sendMessage(Message.PERMISSION.format("selling at signs"));
 				return;
 			}
 			
-			Player hunter = event.getPlayer();
 			plugin.getSellExecutor().sellHeads(hunter, hunter.isSneaking());
+			return;
+		}
+		
+		Integer bountyIndex = (Integer) getBlockData("bounty", block, "index");
+		if(bountyIndex != null && block.getState() instanceof Sign) {
+			// check permission
+			if(!hunter.hasPermission("hunter.sign.bounty"))
+				return;
+			
+			signLinkMap.put(hunter, (Sign) block.getState());
+			hunter.sendMessage(Message.BOUNTY_SIGN_LINK.format());
+			return;
+		}
+		
+		if(signLinkMap.containsKey(hunter)) {
+			if (block.getState() instanceof Skull) {
+				Skull skullBlock = (Skull) block.getState();
+				Sign bountySign = signLinkMap.get(hunter);
+				setBlockData("bounty", bountySign.getBlock(), "head", convert(skullBlock));
+				saveConfig();
+				hunter.sendMessage(Message.BOUNTY_HEAD_LINK.format());
+				requestUpdate();
+			} else
+				hunter.sendMessage(Message.BOUNTY_LINK_ABORT.format());
+			signLinkMap.remove(hunter);
 		}
 	}
 	
@@ -117,6 +154,29 @@ public class SignBlockManager extends BlockConfigAccessor<HeadHunter> implements
 		saveConfig();
 	}
 	
+	private String convert(BlockState state) {
+		if(state == null)
+			return null;
+		String worldUUID = state.getWorld().getUID().toString();
+		String x = Integer.toHexString(state.getX());
+		String y = Integer.toHexString(state.getY());
+		String z = Integer.toHexString(state.getZ());
+		return String.join(";", worldUUID, x, y, z);
+	}
+	
+	private BlockState convert(String data) {
+		if(data == null)
+			return null;
+		String[] split = data.split(";");
+		World world = Bukkit.getWorld(UUID.fromString(split[0]));
+		if(world == null)
+			return null;
+		int x = Integer.valueOf(split[1], 16);
+		int y = Integer.valueOf(split[2], 16);
+		int z = Integer.valueOf(split[3], 16);
+		return (new Location(world, x, y, z)).getBlock().getState();
+	}
+	
 	private class BountySignUpdateRunnable extends BukkitRunnable {
 		private Set<Sign> signs;
 		
@@ -124,6 +184,7 @@ public class SignBlockManager extends BlockConfigAccessor<HeadHunter> implements
 			this.signs = new HashSet<>();
 		}
 		
+		@SuppressWarnings("deprecation")
 		@Override
 		public void run() {
 			if(signs.isEmpty())
@@ -132,8 +193,12 @@ public class SignBlockManager extends BlockConfigAccessor<HeadHunter> implements
 			Iterator<Sign> iterator = signs.iterator();
 			while(iterator.hasNext()) {
 				Sign sign = iterator.next();
+				BlockState blockState = convert((String) getBlockData("bounty", sign.getBlock(), "head"));
+				Skull skull = null;
+				if(blockState instanceof Skull)
+					skull = (Skull) blockState;
 				
-				if(!sign.getChunk().isLoaded())
+				if(!sign.getChunk().isLoaded() || (skull != null && !skull.getChunk().isLoaded()))
 					continue;
 				
 				int index = (int) getBlockData("bounty", sign.getBlock(), "index");
@@ -145,6 +210,16 @@ public class SignBlockManager extends BlockConfigAccessor<HeadHunter> implements
 				if(bounty != null) {
 					victimLine = bounty.getVictim().getName();
 					amountLine = plugin.getDropManager().formatMoney(bounty.getAmount());
+					if(skull != null) {
+						if (plugin.isVersionBefore(1, 13, 0))
+							skull.setOwner(bounty.getVictim().getName());
+						else
+							skull.setOwningPlayer(bounty.getVictim());
+						skull.update();
+					}
+				} else if(skull != null) {
+					skull.setOwner("MHF_Question");
+					skull.update();
 				}
 				
 				sign.setLine(0, ChatColor.translateAlternateColorCodes('&', "&0[&4Wanted&0]"));
